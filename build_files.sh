290930c643a4d942e1c6483fee05f5830c80eb00
Aniket Mukherjee <aniketmukherjee1509@gmail.com>
2022-12-10 01:02:04 +0530

    upgraded requirements file

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 pip3 install -r requirements.txt
 python3.9 manage.py makemigrations chat agents users --noinput
 python3.9 manage.py migrate --noinput
-python3.9 manage.py collectstatic
+python3.9 manage.py collectstatic --noinput --clear
