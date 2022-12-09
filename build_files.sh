38a3fb8f0b6a2669e0cfdf37317be2cf0219a1a1
Aniket Mukherjee <aniketmukherjee1509@gmail.com>
2022-12-10 00:03:17 +0530

    upgraded requirements file

--- build_files.sh
+++ build_files.sh
@@ -2,3 +2,4 @@ pip3 install -r requirements.txt
 python3.9 manage.py collectstatic
 python3.9 manage.py makemigrations chat agents users
 python3.9 manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'password')" | python manage.py shell
