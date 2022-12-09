2b1d90e139acede15a3284eeea9590457550d8b9
Aniket Mukherjee <aniketmukherjee1509@gmail.com>
2022-12-10 00:13:13 +0530

    upgraded requirements file

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
+sudo apt install sqlite-devel
 pip3 install -r requirements.txt
 python3.9 manage.py collectstatic
 python3.9 manage.py makemigrations chat agents users
 python3.9 manage.py migrate
-# echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'password')" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'password')" | python manage.py shell
